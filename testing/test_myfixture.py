de2a49f6d4067c12c696c38c8a25a77975fc6370
shiyi <1335077456@qq.com>
2020-09-06 09:55:04 +0800

    1.fixture 传递参数 2.参数化与fixture结合实现数据驱动

--- testing/test_myfixture.py
+++ testing/test_myfixture.py
@@ -3,10 +3,11 @@ import pytest
 
 class TestMyFixture:
 
-    def test_case1(self,login,getUser):
+    def test_case1(self,login,getUser,request):
         print(login)
         print(getUser)
         print('需要登陆')
 
-    def test_case2(self):
-        print('不需要要登录')
+    @pytest.mark.parametrize('login',[(1,2),(3,4)])
+    def test_case2(self,login):
+        print('login参数化',login)
