5afb692329fc76581385484cfe557491d04f21bc
shiyi <1335077456@qq.com>
2020-12-06 22:59:17 +0800

    调整pytest

--- web/test_tag.py
+++ web/test_tag.py
@@ -5,19 +5,23 @@ import requests
 
 from web.tag import Tag
 
+class TestTag():
 
-def test_tag_list():
-    tag = Tag()
-    r = tag.list()
+    def setup(self):
+        self.tag = Tag()
 
-    tag_name = 'shiyi'+ str(time.time())
-    r = tag.edit(id='etILlkCwAArWGCRm9_YgQuAjdD5GGmDA',tag_name=tag_name)
-    assert r.status_code == 200
-    assert r.json()['errcode'] == 0
-    ## 查询列表判断是否更新成功
-    r = tag.list()
-    tags = [
-            tag for group in r.json()['tag_group'] if group['group_name'] == 'important'
-            for tag in group['tag'] if tag['name'] == tag_name
-    ]
-    assert len(tags) >0
+    def test_tag_list(self):
+        r = self.tag.list()
+
+        tag_name = 'shiyi'+ str(time.time())
+        r = self.tag.edit(id='etILlkCwAArWGCRm9_YgQuAjdD5GGmDA',tag_name=tag_name)
+        assert r.status_code == 200
+        assert r.json()['errcode'] == 0
+        ## 查询列表判断是否更新成功
+        r = self.tag.list()
+        tags = [
+                tag for group in r.json()['tag_group'] if group['group_name'] == 'important'
+                for tag in group['tag'] if tag['name'] == tag_name
+        ]
+        assert len(tags) >0
+
