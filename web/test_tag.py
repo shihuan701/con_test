1a0969fc6259ecc162e0a523d97f2cc289b0d996
shiyi <1335077456@qq.com>
2020-12-06 22:13:58 +0800

    完成企业微信标签编辑接口断言

--- web/test_tag.py
+++ web/test_tag.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 import requests
 
@@ -8,22 +9,29 @@ def test_tag_list():
     corpsecret = 'hDRw6k0Q0lXdfTFsG1Baakf8CzuKuJzdqoo3x-euGnM'
     r = requests.get(url='https://qyapi.weixin.qq.com/cgi-bin/gettoken',
                      params={'corpid':corpid,'corpsecret':corpsecret})
-    print(json.dumps(r.json(), indent=2))
     assert r.status_code==200
     assert r.json()['errcode'] == 0
     token = r.json()['access_token']
     r = requests.post(url='https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list',
                   params={'access_token':token},
                   )
-
     print(json.dumps(r.json(),indent=2))
     assert r.status_code==200
     assert r.json()['errcode'] ==0
-
+    tag_name = 'shiyi'+ str(time.time())
     r = requests.post(url='https://qyapi.weixin.qq.com/cgi-bin/externalcontact/edit_corp_tag',
                       params={'access_token': token},
-                      json ={'id':'etILlkCwAArWGCRm9_YgQuAjdD5GGmDA','name': 'interesting——new'})
+                      json ={'id':'etILlkCwAArWGCRm9_YgQuAjdD5GGmDA','name': tag_name})
 
     print(json.dumps(r.json(), indent=2))
     assert r.status_code == 200
     assert r.json()['errcode'] == 0
+    ## 查询列表判断是否更新成功
+    r = requests.post(url='https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list',
+                      params={'access_token': token},
+                      )
+    tags = [
+            tag for group in r.json()['tag_group'] if group['group_name'] == 'important'
+            for tag in group['tag'] if tag['name'] == tag_name
+    ]
+    assert len(tags) >0
